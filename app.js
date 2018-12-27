2c5093881fb3c583233226dc9749ac9d2cab4462
Hampton <hampmoe@me.com>
2018-12-27 09:02:29 -0500

    More Changes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+require('dotenv').config()
 var express = require('express')
 var app = express()
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var mysql = require('mysql');
 var requestify = require('requestify');
 var jsSha256 = require("js-sha256");
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'herohamp',
-    password: '',
-    database: 'c9'
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
 function makeid(len) {
@@ -24,23 +25,23 @@ function makeid(len) {
 connection.connect()
 
 app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "scratchnotifier.cf");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
 
 app.get('/', function(req, res) {
-    res.send('Bippity Boppity Get Off My Property');
+    res.redirect("https://isonlinev3.docs.apiary.io")
 })
 
 //STATUS PATH
 
-app.get('/status/get/:users', function(req, res) {
+app.get('/v1/status/get/:users', function(req, res) {
     let names = req.params.users.split(",");
     let command = 'SELECT `username`,`date` FROM users WHERE (';
     console.log(names);
     for (var i in names) {
-        command += 'username = ? OR ';
+        command += 'BINARY username = ? OR ';
     }
 
     command = command.slice(0, -4);
@@ -88,8 +89,8 @@ app.get('/status/get/:users', function(req, res) {
 
 })
 
-app.get('/status/set/:user/:userkey', function(req, res) {
-    let command = "UPDATE users SET date = ? WHERE username = ? AND token = ? AND activated = 1;";
+app.get('/v1/status/set/:user/:userkey', function(req, res) {
+    let command = "UPDATE users SET date = ? WHERE BINARY username = ? AND BINARY token = ? AND activated = 1;";
 
     console.log(command);
 
@@ -110,7 +111,7 @@ app.get('/status/set/:user/:userkey', function(req, res) {
 
 // VERIFY PATH
 
-app.get('/verify/gettoken/:username/:passphrase', function(req, res) {
+app.get('/v1/verify/gettoken/:username/:passphrase', function(req, res) {
     let command = "INSERT INTO users (username, activated, date, token) VALUES (?, 1, ?, ?) ON DUPLICATE KEY UPDATE token = ?, activated = 1";
 
     requestify.get('https://api.scratch.mit.edu/projects/274388698/comments?' + Math.random()).then(function(response) {
